bdcd36c9a50ef9554a6573723a8220957c2b8395
Abhishek Srivastava <53976003+abhishek2x@users.noreply.github.com>
2019-12-28 16:30:47 +0530

    Nineth commit

---
+++ refrencepassing1.cpp
@@ -0,0 +1,19 @@
+// Concept of passing by refrence
+
+#include <iostream>
+using namespace std;
+void square(int *pNumber);
+
+int main(){
+    int number = 10;
+    cout << "In main : " << &number << endl; 
+    cout << number << endl;
+    square(&number);
+    cout << number << endl;
+    return 0;
+}
+
+void square(int *pNumber){
+    cout << "In square() : " << pNumber << endl;
+    *pNumber *= *pNumber;
+}
